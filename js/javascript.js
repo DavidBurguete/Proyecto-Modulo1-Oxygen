1fc82d5b3157dfaa6fdd52de7628951cece5bdf1
DavidBurguete <dburgueteg@gmail.com>
2025-02-03 13:53:30 +0100

    Hecho menú desplegable y corregido error de imagen

---
+++ js/javascript.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let menu = document.getElementById("burger_menu");
+    let nav = document.getElementsByTagName("nav")[0];
+    let collapsed = true;
+    menu.addEventListener("click", function(){
+        if(collapsed){
+            nav.classList.add("nav__expanded");
+            menu.style.top = "36.78px";
+            menu.children[0].classList.add("burger_menu--first--crossed");
+            menu.children[1].classList.add("burger_menu--middle--crossed");
+            menu.children[2].classList.add("burger_menu--last--crossed");
+        }
+        else{
+            nav.classList.remove("nav__expanded");
+            menu.style.top = "29px";
+            menu.children[0].classList.remove("burger_menu--first--crossed");
+            menu.children[1].classList.remove("burger_menu--middle--crossed");
+            menu.children[2].classList.remove("burger_menu--last--crossed");
+        }
+        collapsed = !collapsed;
+    });
+});
